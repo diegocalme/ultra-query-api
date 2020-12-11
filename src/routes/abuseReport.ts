5cd2be0fb8c20497143963963a5a3a73a03909c3
Diego Calvo <diegocalme@outlook.com>
2020-12-10 19:37:56 -0600

    feat: refractor of routes/api

---
+++ src/routes/abuseReport.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { getFullReport } from '../services/apipdbService';
+import { allowMultipleIPs } from '../middleware/allowValidNetTarget';
+
+export const router = Router();
+
+router.get('/', allowMultipleIPs, (req, res): void => {
+  getFullReport(req.body.netTarget)
+    .then((report) => {
+      res.status(/*report.status || */200).jsonp(report).end();
+    })
+    .catch((error) => {
+      res.status(/*error.status || */500).jsonp({
+        success: false,
+        error: error.message
+      }).end();
+    });
+});
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import { getFullReport } from '../services/apipdbService';
-import { allowMultipleIPs } from '../middleware/allowValidNetTarget';
+import { router as abuseReportRouter } from './abuseReport';
+import { router as dnsRouter } from './dns';
 
 export const router = Router();
 
-router.get('/abuse-report', allowMultipleIPs, (req, res): void => {
-  getFullReport(req.body.netTarget)
-    .then((report) => {
-      res.status(/*report.status || */200).jsonp(report).end();
-    })
-    .catch((error) => {
-      res.status(/*error.status || */500).jsonp({
-        success: false,
-        error: error.message
-      }).end();
-    });
-});
+router.use('/abuse-report', abuseReportRouter);
+router.use('/dns', dnsRouter);
