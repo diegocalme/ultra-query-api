9bd791f8e3b693cd580f99d95eda62b2a039cc64
Diego Calvo <diegocalme@outlook.com>
2020-12-10 20:44:58 -0600

    feat: createExpressRes implemented in abuseReports

--- src/routes/abuseReport.ts
+++ src/routes/abuseReport.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { createExpressRes } from '../utils/createExpressRes';
 import { getFullReport } from '../services/apipdbService';
 import { allowMultipleIPs } from '../middleware/allowValidNetTarget';
 
@@ -7,17 +8,10 @@ export const router = Router();
 router.get('/', allowMultipleIPs, async (req, res) => {
 
   try {
-
     const report = await getFullReport(req.body.netTarget)
-    res.status(200).jsonp(report).end();
-
+    res.status(200).jsonp(createExpressRes(true, 200, report)).end();
   } catch(error) {
-
-    res.status(500).jsonp({
-      success: false,
-      error: error.message
-    }).end();
-    
+    res.status(500).jsonp(createExpressRes(false, 400, error)).end();
   }
 
 });
