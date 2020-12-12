61587eda6f396dd74bbe1927d862633d46e875dd
Diego Calvo <diegocalme@outlook.com>
2020-12-12 14:48:12 -0600

    feat: add DnsErrors enum and DnsResponse interface

--- src/utils/promiseDns.ts
+++ src/utils/promiseDns.ts
@@ -1,5 +1,22 @@
 import { promisify } from 'util';
 import dns from 'dns';
 
+export enum DnsErrors {
+  ENODATA = 500,
+  ESERVFAIL = 500,
+  ENOTFOUND = 404,
+  EREFUSED = 403,
+  EBADQUERY = 400,
+  ECONNREFUSED = 503,
+  ETIMEOUT = 504,
+  EBADSTR = 400
+}
+
+export interface DnsResponse {
+  data?: string | string[] | object | undefined;
+  code?: string | undefined;
+  [propName: string]: any;
+}
+
 export const resolve = promisify(dns.resolve);
 export const reverse = promisify(dns.reverse);
