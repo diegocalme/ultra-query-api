eb660dc9b247b793b8c8b492b5537a5bfcd905d1
Diego Calvo <diegocalme@outlook.com>
2020-12-12 17:39:49 -0600

    refractor: change in StandardRes interface

--- src/utils/createStandardRes.ts
+++ src/utils/createStandardRes.ts
@@ -2,26 +2,27 @@ export type StandardResPayload = object | any[] | undefined;
 type DefaultErrorConfigs = [boolean, number, StandardResPayload];
 
 export const PRESET_ERR_SRV_MISCONFIG: DefaultErrorConfigs = [false, 500, { error: 'Server misconfiguration!' }];
+export const PRESET_ERR_TARGET_UNREACHABLE: DefaultErrorConfigs = [false, 400, { error: 'Timeout! The target is either dead or unreachable by the host.' }];
 export const PRESET_SRV_ERROR: DefaultErrorConfigs = [false, 500, { error: 'Internal Error!' }];
 
 interface StandardRes {
   success: boolean,
   status: number,
   data: StandardResPayload,
-  code?: string | undefined
+  isApiError?: boolean
 }
 
 const defaultPayload: StandardResPayload = {
   error: 'An unknown error occured!'
 }
 
-export function createStandardRes(success: boolean = false, status: number = 400, payload: StandardResPayload = defaultPayload, code?: string | undefined): StandardRes {
+export function createStandardRes(success: boolean = false, status: number = 400, payload: StandardResPayload = defaultPayload, isApiError?: boolean): StandardRes {
 
   return {
     success,
     status,
     data: payload,
-    code
+    isApiError
   };
 
 }
