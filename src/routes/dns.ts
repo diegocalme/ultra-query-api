1facf03f41bce3effa99b6cddd4a80f80c873594
Diego Calvo <diegocalme@outlook.com>
2020-12-12 14:49:10 -0600

    feat: simplify dnsService & use createStandardRes

--- src/routes/dns.ts
+++ src/routes/dns.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-import { createExpressRes } from '../utils/createExpressRes';
-import { getIpv4, getIpv6, getMx, getHostnames } from '../services/dnsService';
+import { getRecord } from '../services/dnsService';
 import { allowSingleIP, allowSingleDomain } from '../middleware/allowValidNetTarget';
 
 export const router = Router();
@@ -8,53 +7,59 @@ export const router = Router();
 router.get('/ipv4', allowSingleDomain, async(req, res) => {
 
   try {
-    const ipv4 = await getIpv4(req.body.netTarget);
-    res.status(200).jsonp(createExpressRes(true, 200, { data: ipv4 })).end();
+    const result = await getRecord(req.body.netTarget, 'A');
+    res.status(200).jsonp(result).end();
   } catch(error) {
-    res.status(500).jsonp(createExpressRes(false, 400, { error })).end();
+    res.status(500).jsonp(error).end();
   }
 
 });
 
 router.get('/ipv6', allowSingleDomain, async(req, res) => {
+
   try {
-    const ipv6 = await getIpv6(req.body.netTarget);
-    res.status(200).jsonp(createExpressRes(true, 200, { data: ipv6 })).end();
+    const result = await getRecord(req.body.netTarget, 'AAAA');
+    res.status(200).jsonp(result).end();
   } catch(error) {
-    res.status(500).jsonp(createExpressRes(false, 400, { error })).end();
+    res.status(500).jsonp(error).end();
   }
+
 });
 
-router.get('/ip', allowSingleDomain, async(req, res) => {
-  try {
+// router.get('/ip', allowSingleDomain, async(req, res) => {
+//   try {
 
-    const ipv4Promise = getIpv4(req.body.netTarget);
-    const ipv6Promise = getIpv6(req.body.netTarget);
+//     const ipv4Promise = getIpv4(req.body.netTarget);
+//     const ipv6Promise = getIpv6(req.body.netTarget);
 
-    // Waits until both of the IP promises are settled
-    const [ipv4, ipv6] = await Promise.allSettled([ipv4Promise, ipv6Promise]);
+//     // Waits until both of the IP promises are settled
+//     const [ipv4, ipv6] = await Promise.allSettled([ipv4Promise, ipv6Promise]);
 
-    res.status(200).jsonp(createExpressRes(true, 200, { data: [ipv4, ipv6] })).end();
+//     res.status(200).jsonp(createExpressRes(true, 200, { data: [ipv4, ipv6] })).end();
 
-  } catch(error) {
-    res.status(500).jsonp(createExpressRes(false, 400, { error })).end();
-  }
-});
+//   } catch(error) {
+//     res.status(500).jsonp(createExpressRes(false, 400, { error })).end();
+//   }
+// });
 
 router.get('/mx', allowSingleDomain, async(req, res) => {
+
   try {
-    const mx = await getMx(req.body.netTarget);
-    res.status(200).jsonp(createExpressRes(true, 200, { data: mx })).end();
+    const result = await getRecord(req.body.netTarget, 'MX');
+    res.status(200).jsonp(result).end();
   } catch(error) {
-    res.status(500).jsonp(createExpressRes(false, 400, error)).end();
+    res.status(500).jsonp(error).end();
   }
+
 });
 
-router.get('/hostnames', allowSingleIP, async(req, res) => {
+router.get('/reverse', allowSingleIP, async(req, res) => {
+
   try {
-    const hostnames = await getHostnames(req.body.netTarget);
-    res.status(200).jsonp(createExpressRes(true, 200, { data: hostnames })).end();
+    const result = await getRecord(req.body.netTarget, 'hostnames');
+    res.status(200).jsonp(result).end();
   } catch(error) {
-    res.status(500).jsonp(createExpressRes(false, 400, { error })).end();
+    res.status(500).jsonp(error).end();
   }
+
 });
--- src/services/dnsService.ts
+++ src/services/dnsService.ts
@@ -1,21 +1,54 @@
-import { resolve as dnsResolve, reverse as dnsReverse } from '../utils/promiseDns';
+import { resolve as dnsResolve, reverse as dnsReverse, DnsResponse, DnsErrors } from '../utils/promiseDns';
+import { createStandardRes, StandardResPayload, PRESET_SRV_ERROR } from '../utils/createStandardRes';
 
 // IMPORTANT
 // The DNS calls in this file are made using a promisified version
 // of the built-in dns module's methods found in utils/promiseDns
 
-export async function getIpv4(domain: string) {
-  return await dnsResolve(domain, 'A');
+export async function getRecord(netTarget: string, dnsRecord: string) {
+
+  try {
+
+    let response: DnsResponse = await dnsResolve(netTarget, dnsRecord);
+    return createStandardRes(true, 200, response);
+
+  } catch(error) {
+
+    if(error.code) {
+      const httpErrorCode = DnsErrors[error.code] || 400;
+      const payload: StandardResPayload = {
+        error: error.code
+      }
+      // httpErrorCode forced to be number since it comes from an enum
+      throw createStandardRes(false, <number>httpErrorCode, payload);
+    } else {
+      throw createStandardRes(...PRESET_SRV_ERROR);
+    }
+
+  }
 }
 
-export async function getIpv6(domain: string) {
-  return await dnsResolve(domain, 'AAAA');
-}
+export async function getHostnames(netTarget: string) {
 
-export async function getHostnames(ipAddress: string) {
-  return await dnsReverse(ipAddress);
-}
+  // This function was separated from getRecords for clarity, since the
+  // hostnames are not a DNS record per se. Also, because is mostly known
+  // as reverse lookup.
 
-export async function getMx(domain: string) {
-  return await dnsResolve(domain, 'MX');
+  try {
+
+    let response: DnsResponse = await dnsReverse(netTarget);
+    return createStandardRes(true, 200, response);
+
+  } catch(error) {
+
+    if(error.code) {
+      const payload: StandardResPayload = {
+        error: error.code
+      }
+      throw createStandardRes(false, 400, payload);
+    } else {
+      throw createStandardRes(...PRESET_SRV_ERROR);
+    }
+
+  }
 }
