6a66fc62cb2b0a09f219206bbb3d03a5feb4ec6e
Diego Calvo <diegocalme@outlook.com>
2020-12-13 14:21:41 -0600

    fix: (bug) change getRecord to dnsPromise/resolve

--- src/utils/convertNetTarget.ts
+++ src/utils/convertNetTarget.ts
@@ -1,6 +1,6 @@
-import { getRecord } from '../services/dnsService';
+import { resolve as dnsResolve } from './promiseDns';
 
 export async function convertDomainToIp(domain: string) {
-  const ipv4: any = await getRecord(domain, 'A');
+  const ipv4: any = await dnsResolve(domain, 'A');
   return ipv4[0] || undefined;
 }
