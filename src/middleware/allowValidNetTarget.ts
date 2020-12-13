89348a95e41d38964b3045306f0772fdc0e1e6a0
Diego Calvo <diegocalme@outlook.com>
2020-12-12 22:04:01 -0600

    refractor: complete migration to createStandardRes

--- src/middleware/allowValidNetTarget.ts
+++ src/middleware/allowValidNetTarget.ts
@@ -1,41 +1,57 @@
 import { Request, Response, NextFunction } from 'express';
-import CustomErrors from '../utils/CustomErrors';
+import { createStandardRes } from '../utils/createStandardRes';
 import isDomain from '../utils/isDomain';
 import { isIP } from 'net';
+
+function getInvalidNetTarget(netTargetValue: string, netTargetType: string = 'network target') {
+  const payload = {
+    error: `An invalid ${netTargetType} was passed! ${netTargetValue} is not an accepted value.`
+  }
+  return createStandardRes(false, 400, payload);
+}
+
+function getInvalidNetTargetArray(netTargetValue: string, index: number, netTargetType: string = 'network target') {
+  const payload = {
+    error: `An invalid ${netTargetType} was passed in index ${index}! ${netTargetValue} is not an accepted value.`
+  }
+  return createStandardRes(false, 400, payload);
+}
 
 // Allows for one single network target (either IP or FQDN)
 export function allowSingleNetTarget(req: Request, res: Response, next: NextFunction): void {
 
-  if(isDomain(req.body.netTarget) || isIP(req.body.netTarget)) {
-    // Passes to the next middleware
-    next();
-  } else {
-    res.status(400).jsonp(CustomErrors.getInvalidNetTarget(req.body.netTarget)).end();
+  try {
+    if(isDomain(req.body.netTarget) || isIP(req.body.netTarget)) {
+      // Passes to the next middleware
+      next();
+    } else {
+      const response = getInvalidNetTarget(req.body.netTarget);
+      res.status(response.status).jsonp(response).end();
+    }
+  } catch(error) {
+    res.jsonp({}).end();
   }
 
 }
 
 // Allows for one or multiple network targets (either IPs or FQDNs)
 export function allowMultipleNetTarget(req: Request, res: Response, next: NextFunction): void {
+
   if(Array.isArray(req.body.netTarget)) {
 
     // Validates the elements of the array
-    let hasBadInput = false;
-
     for(let i = 0; i < req.body.netTarget.length; i++) {
 
       let netTarget = req.body.netTarget[i];
 
-      if(!isIP(netTarget)) {
-        let errorToReturn = CustomErrors.getInvalidNetTargetArray(req.body.netTarget[i], i, 'IP address');
-        res.status(400).jsonp(errorToReturn).end();
-        hasBadInput = true;
-        break;
+      if(!isIP(netTarget) && !isDomain(netTarget)) {
+        const error = getInvalidNetTargetArray(req.body.netTarget[i], i);
+        res.status(error.status).jsonp(error).end();
+        return;
       }
     }
-
-    // Passes to the next middleware if all the elements are valid
-    if(!hasBadInput) next();
+    
+    next();
 
   } else {
     // A string was passed
@@ -48,63 +64,65 @@ export function allowSingleDomain(req: Request, res: Response, next: NextFunctio
   if(isDomain(req.body.netTarget)) {
     next();
   } else {
-    res.status(400).jsonp(CustomErrors.getInvalidNetTarget(req.body.netTarget, 'domain name')).end();
+    const error = getInvalidNetTarget(req.body.netTarget, 'domain name');
+    res.status(error.status).jsonp(error).end();
   }
 }
 
 // Allows for multiple domain names
-export function allowMultipleDomains(req: Request, res: Response, next: NextFunction): void {
-  if(Array.isArray(req.body.netTarget)) {
-    let hasBadInput = false;
+// export function allowMultipleDomains(req: Request, res: Response, next: NextFunction): void {
+//   if(Array.isArray(req.body.netTarget)) {
+//     let hasBadInput = false;
 
-    for(let i = 0; i < req.body.netTarget.length; i++) {
+//     for(let i = 0; i < req.body.netTarget.length; i++) {
 
-      let netTarget = req.body.netTarget[i];
+//       let netTarget = req.body.netTarget[i];
 
-      if(!isIP(netTarget)) {
-        let errorToReturn = CustomErrors.getInvalidNetTargetArray(req.body.netTarget[i], i, 'domain name');
-        res.status(400).jsonp(errorToReturn).end();
-        hasBadInput = true;
-        break;
-      }
-    }
+//       if(!isIP(netTarget)) {
+//         let errorToReturn = CustomErrors.getInvalidNetTargetArray(req.body.netTarget[i], i, 'domain name');
+//         res.status(400).jsonp(errorToReturn).end();
+//         hasBadInput = true;
+//         break;
+//       }
+//     }
 
-    if(!hasBadInput) next();
-  } else {
-    allowSingleDomain(req, res, next);
-  }
-}
+//     if(!hasBadInput) next();
+//   } else {
+//     allowSingleDomain(req, res, next);
+//   }
+// }
 
 // Allows for one single IP address
 export function allowSingleIP(req: Request, res: Response, next: NextFunction): void {
   if(isIP(req.body.netTarget)) {
     next();
   } else {
-    res.status(400).jsonp(CustomErrors.getInvalidNetTarget(req.body.netTarget, 'IP address')).end();
+    const response = getInvalidNetTarget(req.body.netTarget, 'IP address');
+    res.status(response.status).jsonp(response).end();
   }
 }
 
 // Allows for one or multiple IP addresses
-export function allowMultipleIPs(req: Request, res: Response, next: NextFunction): void {
+// export function allowMultipleIPs(req: Request, res: Response, next: NextFunction): void {
 
-  if(Array.isArray(req.body.netTarget)) {
-    let hasBadInput = false;
+//   if(Array.isArray(req.body.netTarget)) {
+//     let hasBadInput = false;
 
-    for(let i = 0; i < req.body.netTarget.length; i++) {
+//     for(let i = 0; i < req.body.netTarget.length; i++) {
 
-      let netTarget = req.body.netTarget[i];
+//       let netTarget = req.body.netTarget[i];
 
-      if(!isIP(netTarget)) {
-        let errorToReturn = CustomErrors.getInvalidNetTargetArray(req.body.netTarget[i], i, 'IP address');
-        res.status(400).jsonp(errorToReturn).end();
-        hasBadInput = true;
-        break;
-      }
-    }
+//       if(!isIP(netTarget)) {
+//         let errorToReturn = CustomErrors.getInvalidNetTargetArray(req.body.netTarget[i], i, 'IP address');
+//         res.status(400).jsonp(errorToReturn).end();
+//         hasBadInput = true;
+//         break;
+//       }
+//     }
 
-    if(!hasBadInput) next();
+//     if(!hasBadInput) next();
 
-  } else {
-    allowSingleIP(req, res, next);
-  }
-}
+//   } else {
+//     allowSingleIP(req, res, next);
+//   }
+// }
--- src/utils/createStandardRes.ts
+++ src/utils/createStandardRes.ts
@@ -1,16 +1,17 @@
 export type StandardResPayload = object | any[] | undefined;
 type DefaultErrorConfigs = [boolean, number, StandardResPayload];
 
-export const PRESET_ERR_SRV_MISCONFIG: DefaultErrorConfigs = [false, 500, { error: 'Server misconfiguration!' }];
-export const PRESET_ERR_TARGET_UNREACHABLE: DefaultErrorConfigs = [false, 400, { error: 'Timeout! The target is either dead or unreachable by the host.' }];
-export const PRESET_SRV_ERROR: DefaultErrorConfigs = [false, 500, { error: 'Internal Error!' }];
-
 interface StandardRes {
   success: boolean,
   status: number,
   data: StandardResPayload,
   isApiError?: boolean
 }
+
+export const PRESET_ERR_SRV_MISCONFIG: DefaultErrorConfigs = [false, 500, { error: 'Server misconfiguration!' }];
+export const PRESET_ERR_TARGET_UNREACHABLE: DefaultErrorConfigs = [false, 400, { error: 'Timeout! The target is either dead or unreachable by the host.' }];
+export const PRESET_SRV_ERROR: DefaultErrorConfigs = [false, 500, { error: 'Internal Error!' }];
+export const PRESET_RESRC_NOT_FOUND: DefaultErrorConfigs = [false, 404, { error: 'Resource not found' }];
 
 const defaultPayload: StandardResPayload = {
   error: 'An unknown error occured!'
