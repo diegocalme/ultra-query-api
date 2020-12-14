96aa55b512b139b220eb9cbfc1b600c934745c61
Diego Calvo <diegocalme@outlook.com>
2020-12-13 20:19:49 -0600

    fix: wrong error due to DNS request

--- src/services/geoipService.ts
+++ src/services/geoipService.ts
@@ -1,5 +1,6 @@
 import geoip from 'geoip-lite';
 import { createStandardRes, StandardResPayload, PRESET_SRV_ERROR } from '../utils/createStandardRes';
+import { TxtHttpErrors } from '../utils/httpTxtErrorsEnum';
 import { getSingleIP } from '../utils/getValidNetTarget';
 
 export async function getGeolocation(netTarget: string) {
@@ -13,11 +14,21 @@ export async function getGeolocation(netTarget: string) {
       const payload: StandardResPayload = {
         error: 'IP couldn\'t be geolocated. It probably isn\'t assigned or is part of a local network.'
       }
-      throw createStandardRes(false, 400, payload);
+      throw createStandardRes(false, 400, payload, true);
     }
   } catch(error) {
 
-    if(error.isApiError) {
+    if(error.code) {
+
+      // Since it's doing a DNS request at the start, that section can throw an error...
+      const httpErrorCode = TxtHttpErrors[error.code] || 400;
+      const payload: StandardResPayload = {
+        error: error.code
+      }
+
+      throw createStandardRes(false, <number>httpErrorCode, payload);
+
+    } else if(error.isApiError) {
       throw error;
     } else {
       throw createStandardRes(...PRESET_SRV_ERROR);
